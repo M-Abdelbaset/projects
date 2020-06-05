9f9e99aef649e5eb67b4a5cf2086a62826b215cc
m.abdelbaset.dev@gmail.com <m.abdelbaset.dev@gmail.com>
2020-06-05 23:37:41 +0200

    fix

--- micro-services-2/students-service/src/main/java/com/home/studentsservice/resources/StudentsController.java
+++ micro-services-2/students-service/src/main/java/com/home/studentsservice/resources/StudentsController.java
@@ -3,6 +3,7 @@ package com.home.studentsservice.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,12 +17,19 @@ public class StudentsController {
 		return new RestTemplate();
 	}
 	
-	@Autowired
-	private RestTemplate restTemplate;
+	@Autowired Environment env;
+	
+	@Autowired RestTemplate restTemplate;
 	
 	@GetMapping(path="/students")
 	public String getStudentWithGrade() {
 		String portNum = restTemplate.getForObject("http://GRADES-SERVICE/grade", String.class);
 		return portNum;
+	}
+	
+	@GetMapping(path="/props")
+	public String getProps() {
+		String props = env.getProperty("my.property") + " - " + env.getProperty("my.property2") + " - " + env.getProperty("my.propertyX");
+		return props;
 	}
 }
