78515eb31e0b8e473cf3661730785cf014a10f25
m.abdelbaset.dev@gmail.com <m.abdelbaset.dev@gmail.com>
2020-06-06 01:35:14 +0200

    updated

--- micro-services-2/students-service/src/main/java/com/home/studentsservice/resources/StudentsController.java
+++ micro-services-2/students-service/src/main/java/com/home/studentsservice/resources/StudentsController.java
@@ -2,13 +2,15 @@ package com.home.studentsservice.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
+@RestController 
+@RefreshScope
 public class StudentsController {
 
 	@Bean
@@ -30,7 +32,7 @@ public class StudentsController {
 	@GetMapping(path="/props")
 	public String getProps() {
 		String props = env.getProperty("my.property") + " - " + env.getProperty("my.property2") 
-		+ " - " + env.getProperty("my.propertyX") + " - " + env.getProperty("my.property.application") + " " + env.getProperty("my.specific");
+		+ " - " + env.getProperty("my.propertyX") + " - " + env.getProperty("my.property.application") + " - " + env.getProperty("my.specific");
 		return props;
 	}
 }
