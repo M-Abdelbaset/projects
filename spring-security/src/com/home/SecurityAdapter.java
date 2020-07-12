46d0b79245961101ef16794433d4be6468d44cdb
m.abdelbaset.dev@gmail.com <m.abdelbaset.dev@gmail.com>
2020-07-12 14:07:53 +0200

    commit

--- spring-security/src/com/home/SecurityAdapter.java
+++ spring-security/src/com/home/SecurityAdapter.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -59,7 +60,13 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
 			.and()
 			.exceptionHandling().accessDeniedPage("/access-denied");
 	}
-	
+	/*
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+		web.httpFirewall(null);
+	}
+	*/
 	@Bean
 	public CustomUserDetailsService customUserDetailsService() {
 		return new CustomUserDetailsService();
